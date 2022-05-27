317d4a145715b61f1e746bb13c05e898ee43b4a9
YJY1220 <jiyeyu1220@gmail.com>
2022-05-27 21:24:52 +0900

    템플릿 생성

--- rootproject/pybo/views.py
+++ rootproject/pybo/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
-    return HttpResponse("안녕하세요, delievery market에 오신걸 환영합니다.")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
