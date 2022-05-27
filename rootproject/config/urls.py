e0298defb36ef2ca366777a10de37135b59b33e4
YJY1220 <jiyeyu1220@gmail.com>
2022-05-28 01:44:35 +0900

    로그인,로그아웃 폼&기능 구현

--- rootproject/config/urls.py
+++ rootproject/config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pybo import views
 
 
 
@@ -22,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
+    path('', views.index, name='index'),
 ]
