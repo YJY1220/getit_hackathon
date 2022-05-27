ac55c7f1223eb9cabac1c02724c2ecee58f1078f
YJY1220 <jiyeyu1220@gmail.com>
2022-05-28 01:26:14 +0900

    로그인,로그아웃 common 앱 생성

--- rootproject/config/settings.py
+++ rootproject/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfg',
     'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
